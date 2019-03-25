f3aab40f0add055d13f04821a80d053940d0f5c4
Tiffany West <tiffanywest520@gmail.com>
2019-03-25 11:44:29 -0400

    code challenge + other files

---
+++ CodingChallenges/CodingChallenge/src/main/java/com/revature/CodingChallenge/hex.java
@@ -0,0 +1,24 @@
+package com.revature.CodingChallenge;
+
+//- Write a method that will take in an int (whole number base 10) 
+//and return its hexadecimal value (whole number base 16)
+
+public class hex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+				int nums1 =10;
+			String hexvalue = Integer.toHexString(nums1);
+			System.out.println("int " + nums1 + " Hex is " +hexvalue );
+		
+
+					
+		 
+		
+	}
+	
+
+}
